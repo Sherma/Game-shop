c7499fb68742d89100ae75a5896121165b685630
Sasha Nyzhnyk <lesteros555@gmail.com>
2022-04-03 11:09:28 +0300

    Add home page and link to cart

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class HomeController: Controller
+    {
+        private readonly iAllGames _gameRep;
+
+        public HomeController(iAllGames gameRep)
+        {
+            _gameRep = gameRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeGames = new HomeViewModel
+            {
+                favGames = _gameRep.getFavGames
+            };
+            return View(homeGames);
+
+        }
+    }
+}
